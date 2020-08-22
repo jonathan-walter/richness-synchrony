ba3ca1968579b602180dfdc54a2da7a78abdfac2
Lauren Shoemaker <lash1937@gmail.com>
2020-08-22 11:16:28 -0600

    updated script for making supp theory figures

--- theory/supplementary-theory-run/supplement_theory_figures.R
+++ theory/supplementary-theory-run/supplement_theory_figures.R
@@ -1,4 +1,6 @@
 ## This script produces manuscript-quality figures synthesizing empirical and theoretical results.
+## this script closely matches manuscriptFigures.R, but is used to make the analogous
+## figures where dispersal occurs after growth
 
 rm(list=ls())
 
@@ -11,7 +13,7 @@ emp.dat<-read.csv(here("empirical/analysisvars_table.csv"), stringsAsFactors = F
 #load theory data -- this CSV was assembled offline from cluster output. We are providing the
 #results, and simulation model function R script; see manuscript for a description of the
 #distributions parameter values were drawn from.
-thry.dat<-read.csv(here("theory/theory_output.csv"))
+sup.thry.dat<-read.csv(here("theory/supplementary-theory-run/post_disp_theory_output.csv"))
 
 ## ---------------------------------------------------------------------------------------
 ## Figure 1: Is there spatial synchrony in species richness?
@@ -24,13 +26,13 @@ emp.fig1$displayname<-c("HAY","JRG","JRN-BASN","JRN-IBPE","JRN-SUMM","KNZ-UP","K
 bb<-rgb(0,114,178,255,maxColorValue=255)
 bg<-rgb(0,148,115,255,maxColorValue=255)
 
-pdf(here("figures/main/Fig1_richsynch_combined.pdf"), width=6.5, height=3.25)
+pdf(here("figures/supplement/FigS1_richsynch_combined.pdf"), width=6.5, height=3.25)
 
 layout(matrix(c(1,2),nrow=1),widths=c(0.45,0.55))
 
 par(mar=c(1.5,3.7,1.5,1.1), oma=c(1.3,0,0,0), tcl=-0.4, mgp=c(3,0.5,0))
 
-hist(thry.dat$rRichness, breaks=seq(-.1,1,by=.1), freq=TRUE, xlim=c(-2/20,1), 
+hist(sup.thry.dat$rRichness, breaks=seq(-.1,1,by=.1), freq=TRUE, xlim=c(-2/20,1), 
           xlab="", main="", xaxt="n", yaxt="n", ylab="", col="lightgrey")
 axis(side=1, labels = T, at=c(0, .25, .5, .75, 1), pos=0/20)                             
 axis(side=2, at=seq(0,700,100), labels=seq(0,700,100)/2500, las=1, pos=-2/20)
@@ -61,7 +63,7 @@ dev.off()
 ##---------------------------------------------------------------------------------
 ## Figure 2: Theoretical parameter sensitivity
 
-thry.std<-thry.dat[,colnames(thry.dat) %in% 
+thry.std<-sup.thry.dat[,colnames(sup.thry.dat) %in% 
                      c("rRichness","AvgPlotRich","Evenness","Jaccard","Turnover","Npatches","growrt","competition"
                        ,"env_var","disp","patch_het","autocorr")]
 thry.std$AvgPlotRich<-scale(thry.std$AvgPlotRich)
@@ -90,7 +92,7 @@ error.bar <- function(x, y, upper, lower=upper, length=0.1,...){
 pal=c(rgb(0,0,0,255,maxColorValue=255),rgb(230,159,0,255,maxColorValue=255),rgb(86,180,233,255,maxColorValue=255))
 
 
-pdf(here("figures/main/Fig2_thry_effects.pdf"), width=3.25, height=3.25)
+pdf(here("figures/supplement/FigS2_thry_effects.pdf"), width=3.25, height=3.25)
 
 par(mar=c(6.1,3.5,0.7,0.7), tcl=-0.4, mgp=c(1,0.5,0))
 
@@ -106,61 +108,28 @@ legend("topright",fill=pal,legend=c("Abiotic temporal","Abiotic spatial","Demogr
 
 dev.off()
 
-##--------------------------------------------------------------------
-## Figure 3: Empirical effects
-
-emp.std<-emp.dat[,colnames(emp.dat) %in% c("rRichness","organism","extent","n.taxa.regional","Evenness","Jaccard","Turnover","AvgPlotRich")]
-emp.std$extent<-scale(emp.std$extent)
-emp.std$n.taxa.regional<-scale(emp.std$n.taxa.regional)
-emp.std$Evenness<-scale(emp.std$Evenness)
-emp.std$Jaccard<-scale(emp.std$Jaccard)
-emp.std$Turnover<-scale(emp.std$Turnover)
-emp.std$AvgPlotRich<-scale(emp.std$AvgPlotRich)
-
-emp.effects<-lm(rRichness~organism+extent+AvgPlotRich+Evenness+Jaccard+Turnover, data=emp.std)
-summary(emp.effects)
-
-thry.effects2<-lm(rRichness~AvgPlotRich+Evenness+Jaccard+Turnover, data=thry.std)
-summary(thry.effects2)
-
-pdf(here("figures/main/Fig3_emp_effects.pdf"), width=3.25, height=3.25)
-
-par(mar=c(4.1,3.5,0.7,0.7), tcl=-0.4, mgp=c(1,0.5,0))
-
-barplot(rbind(emp.effects$coefficients[2:7], c(NA,NA,thry.effects2$coefficients[-1])), 
-        ylim=c(-0.11, 0.32), names.arg=c("Biome", "Extent",  "Richness", "Evenness", "Beta \n diversity", "Turnover"), 
-        cex.names = .8, las=2, cex.axis = .8, beside=T,
-        col=rep(c("grey40","grey80"),6))
-abline(h=0)
-error.bar(seq(from=1.5,by=3,length.out = 6),emp.effects$coefficients[-1],summary(emp.effects)$coefficients[-1,2],length=0.03)
-error.bar(seq(from=8.5,by=3,length.out = 4), thry.effects2$coefficients[-1],summary(thry.effects2)$coefficients[-1,2],length=0.03)
-mtext("Effect size",2,line=2.2)
-legend("topleft",fill=c("grey40","grey80"),legend=c("Empirical","Theoretical"),bty="n", cex=0.8, inset=c(0.075,0))
-
-dev.off()
-
 ##--------------------------------------------------------------------------
-## Figure 4: community cv ~ richness synchrony and avg richness
+## Figure 3: community cv ~ richness synchrony and avg richness
 
 ptcol<-c(bb,bg,bg,bg,bg,bg,bg,bg,bb,bb,bb,bb,bb,bb,bb,bg,bg,bg,bb,bb)
 
-fit3<-lm(thry.dat$cv~thry.dat$rRichness)
-cor.test(thry.dat$cv,thry.dat$rRichness)
+fit3<-lm(sup.thry.dat$cv~sup.thry.dat$rRichness)
+cor.test(sup.thry.dat$cv,sup.thry.dat$rRichness)
 
 fit4<-lm(emp.dat$CVTotBiomass~emp.dat$rRichness)
 cor.test(emp.dat$CVTotBiomass,emp.dat$rRichness)
 
-cor.test(thry.dat$cv,thry.dat$AvgPlotRich)
-fit5<-lm(thry.dat$cv~thry.dat$AvgPlotRich)
+cor.test(sup.thry.dat$cv,sup.thry.dat$AvgPlotRich)
+fit5<-lm(sup.thry.dat$cv~sup.thry.dat$AvgPlotRich)
 
 fit6<-lm(emp.dat$CVTotBiomass~emp.dat$AvgPlotRich)
 cor.test(emp.dat$CVTotBiomass,emp.dat$AvgPlotRich)
 
-pdf(here("figures/main/Fig4_cv_richsynch.pdf"), width=6.5, height=6.5)
+pdf(here("figures/supplement/FigS3_cv_richsynch.pdf"), width=6.5, height=6.5)
 
 par(mar=c(3.1,3.1,1.5,0.5), tcl=-0.4, mgp=c(1.75,0.5,0), mfcol=c(2,2), oma=c(0,0,0,0.6))
 
-plot(thry.dat$rRichness, thry.dat$cv, xlab="Richness synchrony", ylab="Community CV", xlim=c(0,1), ylim=c(0,1),
+plot(sup.thry.dat$rRichness, sup.thry.dat$cv, xlab="Richness synchrony", ylab="Community CV", xlim=c(0,1), ylim=c(0,1),
      pch=20, col="grey")
 abline(fit3, lwd=2)
 #mtext("Theoretical",3,line=0.2)
@@ -176,7 +145,7 @@ mtext(expression(paste(italic(R)^2,"=0.43, ",italic(p),"=0.002, ",hat(beta),"=0.
 #mtext(expression(paste(italic(r),"=0.65, ",italic(p),"=0.002")),3,line=-1.3,cex=0.9)
 mtext("C)",at=0.01,line=-1.3)
 
-plot(thry.dat$AvgPlotRich, thry.dat$cv, xlab="Richness", ylab="Community CV", ylim=c(0,1), pch=20, col="grey")
+plot(sup.thry.dat$AvgPlotRich, sup.thry.dat$cv, xlab="Richness", ylab="Community CV", ylim=c(0,1), pch=20, col="grey")
 abline(fit5,lwd=2)
 mtext(expression(paste(italic(R)^2,"=0.02, ", hat(beta),"=-0.0006")),3,line=-1.65,cex=0.9)
 #mtext(expression(paste(italic(r),"=-0.15")),3,line=-1.3,cex=0.9)
@@ -191,17 +160,17 @@ mtext("D)",at=4.5,line=-1.3)
 
 dev.off()
 
-# fit3a<-lm(thry.dat$cv~scale(thry.dat$rRichness))
-# fit5a<-lm(thry.dat$cv~scale(thry.dat$AvgPlotRich))
+# fit3a<-lm(sup.thry.dat$cv~scale(sup.thry.dat$rRichness))
+# fit5a<-lm(sup.thry.dat$cv~scale(sup.thry.dat$AvgPlotRich))
 # fit4a<-lm(emp.dat$CVTotBiomass~scale(emp.dat$rRichness))
 # fit6a<-lm(emp.dat$CVTotBiomass~scale(emp.dat$AvgPlotRich))
 
 
-summary(lm(thry.dat$cv~thry.dat$rRichness))
-summary(lm(thry.dat$cv~thry.dat$AvgPlotRich))
-summary(lm(thry.dat$cv~thry.dat$Evenness))
-summary(lm(thry.dat$cv~thry.dat$Turnover))
-summary(lm(thry.dat$cv~thry.dat$Jaccard))
+summary(lm(sup.thry.dat$cv~sup.thry.dat$rRichness))
+summary(lm(sup.thry.dat$cv~sup.thry.dat$AvgPlotRich))
+summary(lm(sup.thry.dat$cv~sup.thry.dat$Evenness))
+summary(lm(sup.thry.dat$cv~sup.thry.dat$Turnover))
+summary(lm(sup.thry.dat$cv~sup.thry.dat$Jaccard))
 
 
 
