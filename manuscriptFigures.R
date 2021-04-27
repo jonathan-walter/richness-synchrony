7ee73a435c5eeb6e6111d5dc11bdfed74e0f85dc
jonathan-walter <jaw3es@virginia.edu>
2021-04-27 16:30:53 -0400

    update figs for revision

--- manuscriptFigures.R
+++ manuscriptFigures.R
@@ -14,7 +14,7 @@ emp.dat<-read.csv(here("empirical/analysisvars_table.csv"), stringsAsFactors = F
 #thry.dat<-read.csv(here("theory/pre_disp_theory_output.csv"))
 
 # local dispersal
-thry.dat<-read.csv(here("theory/local_disp_theory_output_v2.csv"))
+thry.dat<-read.csv(here("theory/newv2_local_params2500_results.csv"))
 
 ## ---------------------------------------------------------------------------------------
 ## Figure 1: Is there spatial synchrony in species richness?
@@ -66,7 +66,7 @@ dev.off()
 
 thry.std<-thry.dat[,colnames(thry.dat) %in% 
                      c("rRichness","AvgPlotRich","Evenness","Jaccard","Turnover","Npatches","growrt","competition"
-                       ,"env_var","disp","patch_het","autocorr")]
+                       ,"env_var","disp","patch_het","autocorr","jacc.tu","jacc.ne")]
 thry.std$AvgPlotRich<-scale(thry.std$AvgPlotRich)
 thry.std$Evenness<-scale(thry.std$Evenness)
 thry.std$Jaccard<-scale(thry.std$Jaccard)
@@ -78,6 +78,8 @@ thry.std$env_var<-scale(thry.std$env_var)
 thry.std$disp<-scale(thry.std$disp)
 thry.std$patch_het<-scale(thry.std$patch_het)
 thry.std$autocorr<-scale(thry.std$autocorr)
+thry.std$jacc.ne<-scale(thry.std$jacc.ne)
+thry.std$jacc.tu<-scale(thry.std$jacc.tu)
 
 param.effects<-lm(rRichness~env_var + autocorr + Npatches + patch_het + growrt + competition + disp, data=thry.std)
 summary(param.effects)
@@ -97,7 +99,7 @@ pdf(here("figures/main/Fig2_thry_effects.pdf"), width=3.25, height=3.25)
 
 par(mar=c(6.1,3.5,0.7,0.7), tcl=-0.4, mgp=c(1,0.5,0))
 
-barplot(param.effects$coefficients[2:8], ylim=c(-.01, .2), names.arg=c("Temporal \n Variation", "Temporal \n Autocorrelation", 
+barplot(param.effects$coefficients[2:8], ylim=c(-.02, .15), names.arg=c("Temporal \n Variation", "Temporal \n Autocorrelation", 
                                                                            "# Patches", "Patch \n Heterogeneity",
                                                                            "Growth \n Rate", "Competitive \n Strength",
                                                                            "Dispersal"), 
@@ -113,7 +115,7 @@ dev.off()
 ## Figure 3: Empirical effects
 
 emp.std<-emp.dat[,colnames(emp.dat) %in% c("rRichness","organism","extent","n.taxa.regional","Evenness","Jaccard",
-                                           "Jacc.tu","Jacc.ne","Turnover","AvgPlotRich")]
+                                           "Jacc.tu","Jacc.ne","Turnover","AvgPlotRich","n.plots")]
 emp.std$extent<-scale(emp.std$extent)
 emp.std$n.taxa.regional<-scale(emp.std$n.taxa.regional)
 emp.std$Evenness<-scale(emp.std$Evenness)
@@ -122,6 +124,7 @@ emp.std$Jacc.tu<-scale(emp.std$Jacc.tu)
 emp.std$Jacc.ne<-scale(emp.std$Jacc.ne)
 emp.std$Turnover<-scale(emp.std$Turnover)
 emp.std$AvgPlotRich<-scale(emp.std$AvgPlotRich)
+emp.std$n.plots<-scale(emp.std$n.plots)
 
 emp.effects<-lm(rRichness~organism+extent+AvgPlotRich+Evenness+Jaccard+Turnover, data=emp.std)
 summary(emp.effects)
@@ -132,17 +135,21 @@ summary(emp.effects2)
 thry.effects2<-lm(rRichness~AvgPlotRich+Evenness+Jaccard+Turnover, data=thry.std)
 summary(thry.effects2)
 
+thry.effects3<-lm(rRichness~AvgPlotRich+Evenness+jacc.tu+jacc.ne+Turnover, data=thry.std)
+summary(thry.effects3)
+
+
 pdf(here("figures/main/Fig3_emp_effects.pdf"), width=3.25, height=3.25)
 
 par(mar=c(4.1,3.5,0.7,0.7), tcl=-0.4, mgp=c(1,0.5,0))
 
 barplot(rbind(emp.effects$coefficients[2:7], c(NA,NA,thry.effects2$coefficients[-1])), 
-        ylim=c(-0.11, 0.32), names.arg=c("Biome", "Extent",  "Richness", "Evenness", "Beta \n diversity", "Turnover"), 
+        ylim=c(-0.11, 0.32), names.arg=c("Biome", "Extent", "Richness", "Evenness", "Beta \n diversity", "Turnover"), 
         cex.names = .8, las=2, cex.axis = .8, beside=T,
         col=rep(c("grey40","grey80"),6))
 abline(h=0)
-error.bar(seq(from=1.5,by=3,length.out = 6),emp.effects$coefficients[-1],summary(emp.effects)$coefficients[-1,2],length=0.03)
-error.bar(seq(from=8.5,by=3,length.out = 4), thry.effects2$coefficients[-1],summary(thry.effects2)$coefficients[-1,2],length=0.03)
+error.bar(seq(from=1.5,by=3,length.out = 7),emp.effects$coefficients[-1],summary(emp.effects)$coefficients[-1,2],length=0.03)
+error.bar(seq(from=8.5,by=3,length.out = 5), thry.effects2$coefficients[-1],summary(thry.effects2)$coefficients[-1,2],length=0.03)
 mtext("Effect size",2,line=2.2)
 legend("topleft",fill=c("grey40","grey80"),legend=c("Empirical","Theoretical"),bty="n", cex=0.8, inset=c(0.075,0))
 
@@ -153,55 +160,55 @@ dev.off()
 
 ptcol<-c(bb,bg,bg,bg,bg,bg,bg,bg,bb,bb,bb,bb,bb,bb,bb,bg,bg,bg,bb,bb)
 
-fit3<-lm(thry.dat$cv~thry.dat$rRichness)
+fit3<-lm(I(-1*thry.dat$cv)~thry.dat$rRichness)
 cor.test(thry.dat$cv,thry.dat$rRichness)
 summary(fit3)
 
-fit4<-lm(I(1/emp.dat$CVTotBiomass)~emp.dat$rRichness)
-cor.test(1/emp.dat$CVTotBiomass,emp.dat$rRichness)
+fit4<-lm(I(-1*emp.dat$CVTotBiomass)~emp.dat$rRichness)
+cor.test(-1*emp.dat$CVTotBiomass,emp.dat$rRichness)
 summary(fit4)
 
-cor.test(thry.dat$cv,thry.dat$AvgPlotRich)
-fit5<-lm(thry.dat$cv~thry.dat$AvgPlotRich)
+cor.test(-1*thry.dat$cv,thry.dat$AvgPlotRich)
+fit5<-lm(I(-1*thry.dat$cv)~thry.dat$AvgPlotRich)
 summary(fit5)
 
-fit6<-lm(I(1/emp.dat$CVTotBiomass)~emp.dat$AvgPlotRich)
-cor.test(1/emp.dat$CVTotBiomass,emp.dat$AvgPlotRich)
+fit6<-lm(I(-1*emp.dat$CVTotBiomass)~emp.dat$AvgPlotRich)
+cor.test(-1*emp.dat$CVTotBiomass,emp.dat$AvgPlotRich)
 summary(fit6)
 
-#### <-------------------- THIS FIGURE NEEDS UPDATING SO TEXT IS CONSISTENT WITH 1/CV ---------------------------
+#### 
 
 pdf(here("figures/main/Fig4_cv_richsynch.pdf"), width=6.5, height=6.5)
 
-par(mar=c(3.1,3.1,1.5,0.5), tcl=-0.4, mgp=c(1.75,0.5,0), mfcol=c(2,2), oma=c(0,0,0,0.6))
+par(mar=c(3.1,3.1,1.75,0.5), tcl=-0.4, mgp=c(1.75,0.5,0), mfcol=c(2,2), oma=c(0,0,0,0.6))
 
-plot(thry.dat$rRichness, thry.dat$cv, xlab="Richness synchrony", ylab="Community CV", xlim=c(0,1), ylim=c(0,1),
+plot(thry.dat$rRichness, -1*thry.dat$cv, xlab="Richness synchrony", ylab="Stability", xlim=c(0,1),
      pch=20, col="grey")
 abline(fit3, lwd=2)
 #mtext("Theoretical",3,line=0.2)
-mtext(expression(paste(italic(R)^2,"=0.48, ",hat(beta),"=0.28")),3,line=-1.65,cex=0.9)
+mtext(expression(paste(italic(R)^2,"=0.22, ",hat(beta),"=-0.29")),3,line=0.1,cex=0.9)
 #mtext(expression(paste(italic(R2),"=0.48, ",italic(beta),"=0.28")),3,line=-1.3,cex=0.9)
-mtext("A)",at=0.01,line=-1.3)
+mtext("A)",at=0.01,line=0.3)
 
-plot(emp.dat$rRichness, 1/emp.dat$CVTotBiomass, xlab="Richness synchrony", ylab="Community CV", xlim=c(0,1), pch=20, col=ptcol, cex=1.75)
+plot(emp.dat$rRichness, -1*emp.dat$CVTotBiomass, xlab="Richness synchrony", ylab="Stability", xlim=c(0,1), pch=20, col=ptcol, cex=1.75)
 abline(fit4,lwd=2)
 #mtext("Empirical",3,line=0.2)
-mtext(expression(paste(italic(R)^2,"=0.65, ",hat(beta),"=0.58")),3,line=-1.65,cex=0.9)
+mtext(expression(paste(italic(R)^2,"=0.42, ",hat(beta),"=-0.58")),3,line=0.1,cex=0.9)
 #mtext(expression(paste(italic(r),"=0.65, ",italic(p),"=0.002, ",italic(beta),"=0.58")),3,line=-1.3,cex=0.9)
-mtext("C)",at=0.01,line=-1.3)
+mtext("C)",at=0.01,line=0.3)
 
-plot(thry.dat$AvgPlotRich, thry.dat$cv, xlab="Richness", ylab="Community CV", ylim=c(0,1), pch=20, col="grey")
+plot(thry.dat$AvgPlotRich, -1*thry.dat$cv, xlab="Richness", ylab="Stability", pch=20, col="grey")
 abline(fit5,lwd=2)
-mtext(expression(paste(italic(R)^2,"=0.02, ",hat(beta),"=-0.0006")),3,line=-1.65,cex=0.9)
+mtext(expression(paste(italic(R)^2,"=0.08, ",hat(beta),"=0.004")),3,line=0.1,cex=0.9)
 #mtext(expression(paste(italic(R2),"=0.02, ",italic(beta),"=-0.0006")),3,line=-1.3,cex=0.9)
-mtext("B)",at=20,line=-1.3)
+mtext("B)",at=2,line=0.3)
 
-plot(emp.dat$AvgPlotRich, 1/emp.dat$CVTotBiomass, xlab="Richness", ylab="Community CV", pch=20, col=ptcol,cex=1.75)
+plot(emp.dat$AvgPlotRich, -1*emp.dat$CVTotBiomass, xlab="Richness", ylab="Stability", pch=20, col=ptcol,cex=1.75)
 abline(fit6,lwd=2)
-mtext(expression(paste(italic(R)^2,"=0.13, ",hat(beta),"=-0.02")),3,line=-1.65,cex=0.9)
+mtext(expression(paste(italic(R)^2,"=0.13, ",hat(beta),"=0.02")),3,line=0.1,cex=0.9)
 #mtext(expression(paste(italic(R),"=-0.37, ",italic(p),"=0.11, ",italic(beta),"=-0.02")),3,line=-1.3,cex=0.9)
-legend("topright",legend=rev(c("Grassland","Marine")),pch=20,col=rev(c(bg,bb)), inset=c(0.03,0.1), cex=1.1)
-mtext("D)",at=4.5,line=-1.3)
+legend("bottomright",legend=rev(c("Grassland","Marine")),pch=20,col=rev(c(bg,bb)), inset=c(0.03,0.1), cex=1.1)
+mtext("D)",at=4.5,line=0.3)
 
 dev.off()
 
@@ -210,18 +217,31 @@ dev.off()
 # fit4a<-lm(emp.dat$CVTotBiomass~scale(emp.dat$rRichness))
 # fit6a<-lm(emp.dat$CVTotBiomass~scale(emp.dat$AvgPlotRich))
 
-summary(lm(thry.dat$cv~thry.dat$rRichness))
-summary(lm(thry.dat$cv~thry.dat$AvgPlotRich))
-summary(lm(thry.dat$cv~thry.dat$Evenness))
-summary(lm(thry.dat$cv~thry.dat$Turnover))
-summary(lm(thry.dat$cv~thry.dat$Jaccard))
+summary(lm(I(-1*thry.dat$cv)~thry.dat$rRichness))
+summary(lm(I(-1*thry.dat$cv)~thry.dat$AvgPlotRich))
+summary(lm(I(-1*thry.dat$cv)~thry.dat$Evenness))
+summary(lm(I(-1*thry.dat$cv)~thry.dat$Turnover))
+summary(lm(I(-1*thry.dat$cv)~thry.dat$Jaccard))
 
 
-summary(lm(emp.dat$CVTotBiomass~emp.dat$rRichness))
-summary(lm(emp.dat$CVTotBiomass~emp.dat$AvgPlotRich))
-summary(lm(emp.dat$CVTotBiomass~emp.dat$Evenness))
-summary(lm(emp.dat$CVTotBiomass~emp.dat$Turnover))
-summary(lm(emp.dat$CVTotBiomass~emp.dat$Jaccard))
-summary(lm(emp.dat$CVTotBiomass~emp.dat$rSppMean))
+summary(lm(I(-1*emp.dat$CVTotBiomass)~emp.dat$rRichness))
+summary(lm(I(-1*emp.dat$CVTotBiomass)~emp.dat$AvgPlotRich))
+summary(lm(I(-1*emp.dat$CVTotBiomass)~emp.dat$Evenness))
+summary(lm(I(-1*emp.dat$CVTotBiomass)~emp.dat$Turnover))
+summary(lm(I(-1*emp.dat$CVTotBiomass)~emp.dat$Jaccard))
+
           
-        
+## use a bootstrapping procedure to check confidence interval on R^2
+
+r2_boot<-NULL
+for(ii in 1:1000){
+  
+  samp<-sample(1:20, 20, replace=TRUE)
+  cv.ii <- -1*emp.dat$CVTotBiomass[samp]
+  turnover.ii <- emp.dat$Turnover[samp]
+  
+  r2_boot<-c(r2_boot, summary(lm(cv.ii~turnover.ii))$r.squared)
+  
+}
+
+quantile(r2_boot, c(0.025,0.5,0.975))
